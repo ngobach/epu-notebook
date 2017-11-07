44891d8b6f5a148cc2222cd75ded6fdf9c736a96
Ngô Xuân Bách <thanbaiks@gmail.com>
2017-11-07 20:01:41 +0700

    Update

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,21 @@ const markdown = require('gulp-markdown');
 const clone = require('gulp-clone');
 const rename = require('gulp-rename');
 const transform = require('gulp-transform');
-/* Tasks */
+const toc = require('gulp-markdown-toc');
 
+const utils = require('./utils');
+
+/* Helper functions */
+function slugify(str) {
+  return require('uslug')(utils.boDau(str));
+}
+
+const renderer = new markdown.marked.Renderer();
+renderer.heading = function(text, level) {
+  return `<h${level} id=${slugify(text)}>${text}</h${level}>`
+}
+
+/* Tasks */
 gulp.task('default', ['build']);
 
 gulp.task('clean', function() {
@@ -20,10 +33,14 @@ gulp.task('clean', function() {
 gulp.task('build', ['clean'], function() {
   const template = fs.readFileSync('./src/template.html').toString('utf8');
   const html = gulp.src('./src/epu-notebook.md')
+    .pipe(toc({
+      slugify: slugify
+    }))
     .pipe(markdown({
       highlight: function(code, lang) {
         return hightlight.highlight(lang || 'cpp', code).value;
-      }
+      },
+      renderer: renderer,
     }))
     .pipe(transform('utf8', function (content, file) {
       return template
